fea3c9541afc97ce9807829eafedb0d86ebc19a8
lucas <lucas.dumy@epitech.eu>
2019-10-16 17:32:54 +0100

    Adding Dale's work to the robot branch

---
+++ icub/body/rightArm/build/CMakeFiles/rightArm.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/rightArm.dir/moveRightArm.cpp.o"
+  "rightArm.pdb"
+  "rightArm"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/rightArm.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
