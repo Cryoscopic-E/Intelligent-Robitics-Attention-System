c0ca57c590cdea276a9a5b59858b42b34dc5c1fd
Emanuele <theblacknightburn@gmail.com>
2019-10-17 16:20:57 +0100

    Little Refactor

--- icub/connect_yarpviews.sh
+++
@@ -1,4 +0,0 @@
-yarp connect /img_proc/threshold /img_proc/th
-yarp connect /img_proc/sobel /img_proc/sd
-yarp connect /img_proc/face /img_proc/fd
-yarp connect /img_proc/marker /img_proc/md
--- icub/launch_yarpviews.sh
+++ icub/launch_yarpviews.sh
@@ -1,4 +1,11 @@
+#! /bin/sh
+
 yarpview --name /img_proc/th --x 100 --y 100 &
 yarpview --name /img_proc/sd --x 500 --y 100 &
 yarpview --name /img_proc/fd --x 100 --y 500 &
-yarpview --name /img_proc/md --x 500 --y 500 
+yarpview --name /img_proc/md --x 500 --y 500 &
+sleep 1s
+yarp connect /img_proc/threshold /img_proc/th
+yarp connect /img_proc/sobel /img_proc/sd
+yarp connect /img_proc/face /img_proc/fd
+yarp connect /img_proc/marker /img_proc/md
